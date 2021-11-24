5ab60cc0c6cc6f7786deced8f2510de89b7496fd
tatitakan <tatitakan43@gmail.com>
2021-11-24 13:21:34 +0700

    first commit

---
+++ LAB3/hello-react/src/components/name.js
@@ -0,0 +1,11 @@
+function MyName() {
+    return (
+    <div className="MyName">
+    <div>ททิทากานต์ อาจทวีกุล
+    </div>
+    <div>610112418010</div>
+    </div>
+
+    );
+}
+export default MyName;
